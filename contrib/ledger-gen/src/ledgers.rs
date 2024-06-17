61d72fe214a5835873ba8e083b36167136d17170
Kunal Bhargava <kbhargava@jumptrading.com>
2024-06-17 20:58:28 +0000

    create nonce account

--- contrib/ledger-gen/src/ledgers.rs
+++ contrib/ledger-gen/src/ledgers.rs
@@ -1,7 +1,12 @@
 use {
     solana_sdk::{
         signature::{Keypair, Signer},
-        commitment_config::{CommitmentConfig}
+        commitment_config::{CommitmentConfig},
+        nonce::{State as NonceState, state::Versions as NonceVersions},
+        system_instruction,
+        system_program,
+        message::Message,
+        transaction::Transaction,
     },
     solana_client::{
         rpc_client::{RpcClient},
@@ -9,6 +14,7 @@ use {
     std::{
         sync::Arc,
     },
+    solana_rpc_client_nonce_utils::{get_account_with_commitment, nonblocking},
 };
 
 use crate::programs;
@@ -96,4 +102,80 @@ pub fn deploy_invoke_same_slot(client: &RpcClient, arc_client: &Arc<RpcClient>, 
     println!("Invoked Program Signature: {:?} - Slot: {:?}", transaction.signatures[0], client.get_slot_with_commitment(CommitmentConfig::processed()).unwrap());
 
     println!("Program Id: {:?}", program_account.pubkey());
+}
+
+pub fn create_nonce_account(client: &RpcClient, payer: &Keypair) {
+    let blockhash = client.get_latest_blockhash().unwrap();
+    let (nonce_account, create_nonce_instructions) = programs::create_nonce_account_instructions(None, &payer, 2000000);
+    let transaction = utils::create_message_and_sign(&create_nonce_instructions, &payer, vec![&payer, &nonce_account], blockhash);
+    let _ = client.send_and_confirm_transaction(&transaction).unwrap();
+    println!("Created Nonce Account: {:?} - Slot: {:?}", nonce_account.pubkey(), client.get_slot_with_commitment(CommitmentConfig::processed()).unwrap());
+
+    let nonce_blockhash = match get_account_with_commitment(client, &nonce_account.pubkey(), CommitmentConfig::processed())
+        .and_then(|ref a| nonblocking::state_from_account(a)).unwrap()
+    {
+        NonceState::Initialized(ref data) => data.blockhash(),
+        _ => panic!("Nonce Account not Initialized"),
+    };
+    println!("Nonce Blockhash: {:?}", nonce_blockhash);
+
+    let new_account = Keypair::new();
+
+    let minimum_balance = client
+        .get_minimum_balance_for_rent_exemption(NonceState::size())
+        .unwrap();
+
+    let open_account_instruction = system_instruction::create_account(
+        &payer.pubkey(),
+        &new_account.pubkey(),
+        minimum_balance,
+        0,
+        &system_program::id(),
+    );
+
+    let message = Message::new_with_nonce(
+        vec![open_account_instruction],
+        Some(&payer.pubkey()),
+        &nonce_account.pubkey(),
+        &payer.pubkey(),
+    );
+    let mut transaction = Transaction::new_unsigned(message);
+    let _ = transaction.try_sign(&vec![&payer, &new_account], nonce_blockhash).unwrap();
+    let _ = client.send_and_confirm_transaction(&transaction).unwrap();
+
+    utils::wait_atleast_n_slots(&client, 2);
+
+    // will fail if we don't update the nonce blockhash
+    let nonce_blockhash = match get_account_with_commitment(client, &nonce_account.pubkey(), CommitmentConfig::processed())
+        .and_then(|ref a| nonblocking::state_from_account(a)).unwrap()
+    {
+        NonceState::Initialized(ref data) => data.blockhash(),
+        _ => panic!("Nonce Account not Initialized"),
+    };
+    println!("Nonce Blockhash: {:?}", nonce_blockhash);
+
+    let new_account = Keypair::new();
+
+    let minimum_balance = client
+        .get_minimum_balance_for_rent_exemption(NonceState::size())
+        .unwrap();
+
+    let open_account_instruction = system_instruction::create_account(
+        &payer.pubkey(),
+        &new_account.pubkey(),
+        minimum_balance,
+        0,
+        &system_program::id(),
+    );
+
+    let message = Message::new_with_nonce(
+        vec![open_account_instruction],
+        Some(&payer.pubkey()),
+        &nonce_account.pubkey(),
+        &payer.pubkey(),
+    );
+    let mut transaction = Transaction::new_unsigned(message);
+    let _ = transaction.try_sign(&vec![&payer, &new_account], nonce_blockhash).unwrap();
+    let _ = client.send_and_confirm_transaction(&transaction).unwrap();
+
 }
